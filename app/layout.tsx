5ebfeb325d6aa8ae8fba2adbc8bdd0b667081b31
SwynnBliz <swynnbliz09@gmail.com>
2024-11-19 15:41:25 +0800

    fixing vercel favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
   title: "Taberna Concur",
   description: "An Online Community for Bartenders",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -11,6 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;}>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         {children}
       </body>
