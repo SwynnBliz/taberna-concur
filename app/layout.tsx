dfd73c008e5e205e7ac4b90d9e76a9a143069bcd
SwynnBliz <swynnbliz09@gmail.com>
2024-11-19 15:24:31 +0800

    returned favicon to app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,6 @@ export default function RootLayout({
   children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body>
         {children}
       </body>
