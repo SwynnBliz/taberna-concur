66361f7d082df142891ee9473ec23b5f3116f966
SwynnBliz <swynnbliz09@gmail.com>
2024-11-19 15:14:44 +0800

    testing favicon in vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export default function RootLayout({
   children: React.ReactNode;}>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body>
         {children}
       </body>
